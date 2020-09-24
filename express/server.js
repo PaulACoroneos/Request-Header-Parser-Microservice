439107c16862afc4282c28681616b0e45e5ae339
Paul Coroneos <paul@pcoroneos.com>
2020-09-24 01:38:54 -0500

    push to netlify

--- express/server.js
+++ express/server.js
@@ -2,31 +2,39 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
+const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
+
+var router = express.Router();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+app.use(bodyParser.json());
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+router.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+router.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+router.get("/api/whoami", function (req, res) {
+  res.json({ipaddress: req.ip, language:req.headers["accept-language"], software:req.headers["user-agent"]});
 });
+
+app.use("/.netlify/functions/server", router); // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
